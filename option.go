382461d7530af87130629aabe44cecccbb4604bf
Javad <ja7ad@live.com>
2023-01-26 09:58:34 +0330

    add: option custom stacktrace level

--- option.go
+++ option.go
@@ -1,17 +1,34 @@
 package zapper
 
-type Option func(*Zapper)
+import (
+	"go.uber.org/zap/zapcore"
+)
 
-// WithMinLevel set minimum level logger
-func WithMinLevel(min Level) Option {
-	return func(z *Zapper) {
-		z.level = min
+type Option func(*Zap)
+
+// WithDebugLevel enable debug level for logging
+func WithDebugLevel() Option {
+	return func(z *Zap) {
+		z.level = func(lvl zapcore.Level) bool {
+			return lvl <= Fatal.zapLevel()
+		}
 	}
 }
 
 // WithTimeFormat set custom time format for zapper logs
 func WithTimeFormat(format TimeFormat) Option {
-	return func(z *Zapper) {
+	return func(z *Zap) {
 		z.timeFormat = format
+	}
+}
+
+// WithCustomStackTraceLevel set custom level for show stacktrace, min level warn
+func WithCustomStackTraceLevel(level Level) Option {
+	return func(z *Zap) {
+		if level < Warn {
+			level = Error
+		}
+
+		z.stackLevel = level
 	}
 }
